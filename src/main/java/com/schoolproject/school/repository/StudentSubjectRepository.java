e020e0bef5b4ce586b99815e0fc91531010618d5
William Ventura <goregrindcore95@gmail.com>
2022-10-12 06:32:36 -0600

    add student enpoints

--- src/main/java/com/schoolproject/school/repository/StudentSubjectRepository.java
+++ src/main/java/com/schoolproject/school/repository/StudentSubjectRepository.java
@@ -2,10 +2,13 @@ package com.schoolproject.school.repository;
 
 import com.schoolproject.school.entity.StudentSubject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.io.Serializable;
 
 @Repository
 public interface StudentSubjectRepository extends JpaRepository<StudentSubject, Integer> {
+    @Modifying
+    @Query(value = "DELETE FROM tbl_student_subject WHERE user_id = ?1", nativeQuery = true)
+    int deleteAllByStudentId(int studentId);
 }
